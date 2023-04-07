793c21f7be40ff50b9a2dce67ccb504ea2b4788b
Kelvin Obioha <obiohakelvin96@gmail.com>
2023-04-07 14:48:15 +0100

    update education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Chrono } from 'react-chrono';
 import { Container } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
 import Fade from 'react-reveal';
 import { ThemeContext } from 'styled-components';
 import endpoints from '../constants/endpoints';
@@ -9,12 +10,44 @@ import Header from './Header';
 import FallbackSpinner from './FallbackSpinner';
 import '../css/education.css';
 
+// styles properties section
+const styles = {
+  introTextContainer: {
+    whiteSpace: 'pre-wrap',
+  },
+};
+
+const renderItems = (items) => (
+  <div>
+    {items.map((item) => (
+      <div key={item.name}>
+        <br />
+        <div>
+          <div style={{ arginzLeft: '10px' }}>{item.year}</div>
+          <a
+            href={item.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: 'blue', textDecoration: 'none' }}
+          >
+            {item.name}
+          </a>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 function Education(props) {
   const theme = useContext(ThemeContext);
   const { header } = props;
   const [data, setData] = useState(null);
   const [width, setWidth] = useState('50vw');
   const [mode, setMode] = useState('VERTICAL_ALTERNATING');
+  const renderIntro = (intro) => (
+    <h4 style={styles.introTextContainer}>
+      <ReactMarkdown children={intro} />
+    </h4>
+  );
 
   useEffect(() => {
     fetch(endpoints.education, {
@@ -44,7 +77,7 @@ function Education(props) {
       <Header title={header} />
       {data ? (
         <Fade>
-          <div style={{ width }} className="section-content-container">
+          <div style={{ width, fontType: 'Roboto', fontSize: '13.5px' }} className="section-content-container">
             <Container>
               <Chrono
                 hideControls
@@ -61,20 +94,32 @@ function Education(props) {
                   titleColor: theme.chronoTheme.titleColor,
                 }}
               >
-                <div className="chrono-icons">
-                  {data.education.map((education) => (education.icon ? (
-                    <img
-                      key={education.icon.src}
-                      src={education.icon.src}
-                      alt={education.icon.alt}
-                    />
-                  ) : null))}
-                </div>
+                <div />
               </Chrono>
             </Container>
           </div>
+          {data ? (
+            <div className="section-content-container">
+              <Fade>
+                {/* Render certificate section */}
+                <Container>
+                  {renderIntro(data.intro)}
+                  {data['Trainings and Certifications']?.map((section) => (
+                    <div key={section.title} style={{ display: 'inline-block', fontSize: '20px' }}>
+                      <h1>{section.title}</h1>
+                      {renderItems(section.items)}
+                    </div>
+                  ))}
+                </Container>
+              </Fade>
+            </div>
+          ) : (
+            <FallbackSpinner />
+          )}
         </Fade>
-      ) : <FallbackSpinner /> }
+      ) : (
+        <FallbackSpinner />
+      )}
     </>
   );
 }
